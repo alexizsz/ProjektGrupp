8a524c81f3b87506f72c8b4e090ec66680192ec4
unknown <matteus.vinasco@stud.sti.se>
2024-04-03 11:40:27 +0200

    Added AboutUs

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,9 +1,24 @@
 import '../App.css'
+import '../components/AboutUs.css'
+
 
 const AboutUs=()=>{
+    
+    
+    
+    
     return(
-        <div>
-        <p>This will be the about us page</p>
+        <div className="about-us-container">
+            <h2 className="about-us-heading">About Us</h2>
+            <div className="about-us-content"></div>
+        <h2>About Us</h2>
+            <p>Welcome to our food and cocktails website!</p>
+            <p>At [Food and Cocktails], we're passionate about serving delicious food and refreshing cocktails to our customers.</p>
+            <p>Our mission is to provide you with exceptional dining experiences, whether you're dining in with us or enjoying our offerings from the comfort of your home.</p>
+            <p>Explore our menu to discover a wide range of mouthwatering dishes and expertly crafted cocktails, all made with the finest ingredients and a whole lot of love.</p>
+            <p>Thank you for choosing [Food and Cocktails]. We look forward to serving you!</p>
+
+
         </div>
     );
 }
